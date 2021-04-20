502e6cccad29a59ffed6e70a8cbec7e34606d24c
alsuqiamneh <81607334+alsuqiamneh@users.noreply.github.com>
2021-04-19 19:34:53 -0700

    Update mult_test.hpp

--- mult_test.hpp
+++ mult_test.hpp
@@ -26,8 +26,8 @@ TEST(MultTest, MultEvaluateOneOp) {
 
 TEST(MultTest, MultEvaluateNothingPassed) {
      Mult* mult = new Mult();
-     EXPECT_EQ(mult->evaluate(), 1.0);
-     EXPECT_EQ(mult->stringify(), "(1.00 * 1.00)");
+     EXPECT_EQ(mult->evaluate(), 0.0);
+     EXPECT_EQ(mult->stringify(), "(0.00 * 1.00)");
 }
 
 TEST(MultTest, MultEvaluateNegOp) {
